7ff0b49ec0740e193d33adbcecd540bc4bbc884a
Shamble94 <fratus.nicholas@gmail.com>
2023-12-20 16:58:32 +0100

    Esercizio base completato

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue;
+
+
+createApp({
+    data(){
+        return{
+            /* ARRAY VUOTO */
+            email_list: []
+        }
+    },
+    methods:{
+        /* FUNZIONE CHE GENERA 10 EMAIL CASUALI */
+        generateMail() {
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((result) => {
+                    /* PUSH DEL RISULTATO NELL'ARRAY */
+                    this.email_list.push(result.data.response);
+                
+                });
+            }
+        }
+    },
+}).mount(`#app`);
